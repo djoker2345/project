aa6aac2927f49a1a2f0eb5e69a430d7530652fb1
Sasha <djoker2352@gmail.com>
2022-06-17 14:14:22 +0300

    lessons 13

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
-const numberOfFilms = +prompt ('How many films you see?' , '' );
+"use strict";
 
-const personalMovieDB = {
-    const: numberOfFilms,
-    movies: {},
-    actors: {},
-    ganers: [],
-    privet: false
-};
+if (2){
+    console.log('Ok!');
+} else{
+    console.log('Error');
+}
 
-const a = prompt('Last see films' , ''),
-      b = prompt('How point you take?' , ''),
-      c = prompt('Last see films' , ''),
-      d = prompt('How point you take?' , '');
+// const num = 50;
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Many');
+// } else {
+//     console.log('Ok!');
+// } 
+// (num === 50) ? console.log('Ok!') : console.log('Error');
 
-personalMovieDB.movies [a] = b;
-personalMovieDB.movies [c] = d; 
-
-console.log(personalMovieDB);
+const num = 54;
+switch (num) {
+    case 49:
+        console.log('wrong!');
+        break;
+    case 100:
+        console.log('Wrong!');
+        break;
+    case 50:
+        console.log('В точку');
+        break;
+    default:
+        console.log('Не в этот раз!');
+        break;
+}
