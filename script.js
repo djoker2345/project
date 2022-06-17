ad2e31fb984aa1dbcb6fdb9eed42226d9babc8bc
Sasha <djoker2352@gmail.com>
2022-06-17 18:39:29 +0300

    lessons14

--- script.js
+++ script.js
@@ -1,33 +1,22 @@
 "use strict";
 
-if (2){
-    console.log('Ok!');
-} else{
-    console.log('Error');
-}
+let num = 50;
 
-// const num = 50;
-// if (num < 49) {
-//     console.log('Error');
-// } else if (num > 100) {
-//     console.log('Many');
-// } else {
-//     console.log('Ok!');
-// } 
-// (num === 50) ? console.log('Ok!') : console.log('Error');
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
 
-const num = 54;
-switch (num) {
-    case 49:
-        console.log('wrong!');
-        break;
-    case 100:
-        console.log('Wrong!');
-        break;
-    case 50:
-        console.log('В точку');
-        break;
-    default:
-        console.log('Не в этот раз!');
-        break;
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+for (let i = 1; i < 10; i++) {
+    if( i === 6){
+        // break;
+        continue;
+    }
+    console.log(i)
 }
