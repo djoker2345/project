5913238fd7330cb2d108982605345aa474ee27bc
Sasha <djoker2352@gmail.com>
2022-06-18 17:08:26 +0300

    labrory 2

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
 "use strict";
 
-let num = 50;
+const numberOfFilms = prompt ('Сколько фильмов ты посмотрел' , '' );
 
-// while (num <= 55) {
-//     console.log(num);
-//     num++;
-// }
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    ganers: [],
+    privet: false
+};
 
-// do {
-//     console.log(num);
-//     num++;
-// }
-// while (num < 55);
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Последний просмотреный фильм' , ''),
+          b = prompt('На сколько ты его оценишь?' , ''); 
 
-for (let i = 1; i < 10; i++) {
-    if( i === 6){
-        // break;
-        continue;
+    
+    if (a != null && b != null && a != '' && b != '' && a.length < 50 ) {
+    personalMovieDB.movies[a] = b ;
+    console.log("done");
+    } else {
+    console.log("error");
+    i--;
     }
-    console.log(i)
 }
+
+if (personalMovieDB.count < 10)  {
+    console.log('Просмотрено довольно мало');
+} else if (personalMovieDB.count >10 && personalMovieDB <30) {
+    console.log('Вы прикрасный зритель');
+} else if(personalMovieDB.count >= 30 ) {
+    console.log ('Вы киноман');
+} else {
+    console.log('ошибка');
+} 
+console.log(personalMovieDB);
